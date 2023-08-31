c99545316fb933425d09d3b7fe5e8af41c86e524
Buks ᥫ᭡ <121068378+bukironke@users.noreply.github.com>
2023-09-01 00:39:13 +0100

    Update stockanalysis.r

--- R/stockanalysis.r
+++ R/stockanalysis.r
@@ -1,1 +1,23 @@
+# Install and load necessary libraries
+install.packages("quantmod")
+library(quantmod)
 
+# Define the stock symbol and date range
+stock_symbol <- "AAPL"
+start_date <- as.Date("2020-01-01")
+end_date <- as.Date("2021-12-31")
+
+# Download stock price data
+getSymbols(stock_symbol, from = start_date, to = end_date)
+
+# Calculate daily returns
+daily_returns <- dailyReturn(Cl(get(stock_symbol)))
+
+# Calculate basic statistics
+summary(daily_returns)
+
+# Create a time series plot of stock prices
+chartSeries(get(stock_symbol), theme = "white")
+
+# Create a histogram of daily returns
+hist(daily_returns, breaks = 30, main = "Daily Returns Histogram", xlab = "Returns")
