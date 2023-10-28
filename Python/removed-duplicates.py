bd01b182a50a37324f670b72d10141a49997a54f
Buks ᥫ᭡ <121068378+bukironke@users.noreply.github.com>
2023-10-28 22:21:01 +0100

    Update removed-duplicates.py

--- Python/removed-duplicates.py
+++ Python/removed-duplicates.py
@@ -1,15 +1,23 @@
+#node - single element in data structure that contains both data and pointer reference
+
+#defines class repping node in single linked list - node-value = val, reference = next
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
 def deleteDuplicates(head: ListNode) -> ListNode:
+    # Removes duplicate nodes from a singly linked list.
+    # Traverses the list, adjusting pointers to skip duplicate nodes.
     current = head
     
     while current and current.next:
+# Checks for duplicate values in consecutive nodes.
         if current.val == current.next.val:
+# Adjusts the pointer to skip the duplicate node.
             current.next = current.next.next
         else:
+# Moves to the next node in the linked list.
             current = current.next
 
     return head
